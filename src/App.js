b118ea2ae254f9d04427eba62274b8fd3d60bb00
Jenel <jenelesteron01@gmail.com>
2025-06-09 23:41:15 +0800

    Improve apptext

--- src/App.js
+++ src/App.js
@@ -10,14 +10,25 @@ function App() {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
-        height: '100vh',
-        width: '100%',
+        paddingTop: '50px',
+        minHeight: '100vh',
+        width: '100%',   // Takes full width of its parent
+        display: 'flex', // Example: for centering content over the image
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        textShadow: '2px 2px 4px rgba(0,0,0,0.7)'
       }}
     >
-      <header className="App-Text">
-        <p>Hello</p>
-        <h1>I'm Jenel</h1>    
-      </header>
+      <div class="text-container">
+          <div class="text-item">Hello</div>
+          <div class="text-item">I'm Jenel</div>
+          <p class="sub-gradient-text">
+          <p> A computer engineer, I'm not just a problem-solver; </p>
+          <p> I'm an architect of innovation. I thrive on transforming </p>
+          <p> complex challenges into elegant, efficient digital solutions. </p>
+      </p>
+      </div>
     </div>
   );
 }
